060c94f8be723bf563920edddf3e21d739071337
tudoriosif <tudor_iosif@yahoo.com>
2022-02-24 21:48:36 +0200

    right hero icon

--- frontend/src/components/Login/RightHero.js
+++ frontend/src/components/Login/RightHero.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Box } from '@mui/system';
-// import Carousel from 'react-material-ui-carousel';
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
+import HeroIcon from '../../assets/hero-icon.svg';
 
 const RightHero = () => {
     return (
         <Box sx={{ bgcolor: '#393e41', height: '100vh', width: '50%' }}>
-            {/* <Carousel
-                index={1} // <-- This controls the activeChild
-                autoPlay={false} // <-- You probaly want to disable this for our purposes
-                navButtonsAlwaysVisible>
-                {[1, 2, 3].map((i) => {
-                    return (
-                        <Typography align="center" key={i}>
-                            Child {i}
-                        </Typography>
-                    );
-                })}
-            </Carousel> */}
+            <Stack>
+                <Box
+                    component="img"
+                    sx={{
+                        height: '40%',
+                        width: '40%',
+                        maxHeight: { xs: 350, md: 550 },
+                        maxWidth: { xs: 350, md: 550 },
+                        minWidth: { xs: 250, md: 450 },
+                        minHeight: { xs: 250, md: 450 }
+                    }}
+                    alt="Multimodal"
+                    src={HeroIcon}
+                />
+                <Typography variant="h3" noWrap component="div">
+                    {' '}
+                    Multimodal biometric authentication system
+                </Typography>
+            </Stack>
         </Box>
     );
 };
