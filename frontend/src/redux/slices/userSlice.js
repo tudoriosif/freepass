362721d62e811951d57fe0d9e0c6b129b59af27d
tudoriosif <tudor_iosif@yahoo.com>
2022-06-30 19:57:51 +0300

    updateAccount

--- frontend/src/redux/slices/userSlice.js
+++ frontend/src/redux/slices/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { login, register } from '../thunks/user';
+import { login, register, updateAccount } from '../thunks/user';
 import { checkFinger, emptyFinger, scanFinger } from './fingerSlice';
 import { checkPhoto, sendPhoto } from './photoSlice';
 
@@ -13,24 +13,9 @@ const initialState = {
     faceToken: '',
     fingerToken: '',
     hasFace: false,
-    hasFigner: false,
+    hasFinger: false,
     error: '',
     loading: false
-    // id: '625c68ca786ab3bc6e723b72',
-    // email: 'tudor_iosif@yahoo.com',
-    // role: 'MAIN',
-    // systemID: '58a74bf7-257e-4ccf-b75c-651b7752bf27',
-    // noSystem: 1,
-    // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjI1YzY4Y2E3ODZhYjNiYzZlNzIzYjcyIiwiZW1haWwiOiJ0dWRvcl9pb3NpZkB5YWhvby5jb20iLCJzeXN0ZW1JRCI6IjU4YTc0YmY3LTI1N2UtNGNjZi1iNzVjLTY1MWI3NzUyYmYyNyIsInJvbGUiOiJNQUlOIiwibm9TeXN0ZW0iOjEsImhhc0ZhY2UiOnRydWUsImhhc0ZpbmdlciI6dHJ1ZX0sImlhdCI6MTY1MjU1OTQ4M30.tiqIYBF0d9ntI6U0XxNWryIGdLYsD-zTsgI5YYR5INg',
-    // faceToken:
-    //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjI1YzY4Y2E3ODZhYjNiYzZlNzIzYjcyIiwiZW1haWwiOiJ0dWRvcl9pb3NpZkB5YWhvby5jb20iLCJwYXRoIjoiLi9zcmMvZmFjZXMvdHVkb3JfaW9zaWZAeWFob28uY29tL2NoZWNrIiwicmVzdWx0cyI6MjB9LCJpYXQiOjE2NTI1NTk0ODV9.9OH3WCvstoGE2KzkoGqgWPUWrcUUZ5BXuwo3v29o8Dw',
-    // fingerToken:
-    //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjI1YzY4Y2E3ODZhYjNiYzZlNzIzYjcyIiwiZW1haWwiOiJ0dWRvcl9pb3NpZkB5YWhvby5jb20iLCJjb25maWRlbmNlIjoyMH0sImlhdCI6MTY1MjU1OTQ5MX0.xGvLeWE93JDzENY8Mi30lD4K5bcwAHslBiBfMJkv4Yw',
-    // hasFace: true,
-    // hasFigner: false,
-    // error: '',
-    // loading: false,
-    // hasFinger: true
 };
 
 const userSlice = createSlice({
@@ -56,11 +41,22 @@ const userSlice = createSlice({
         [checkFinger.pending]: (state, action) => {
             state.loading = true;
         },
+        [updateAccount.pending]: (state, action) => {
+            state.loading = true;
+        },
         [login.fulfilled]: (state, action) => {
             localStorage.setItem('token', action.payload.token);
             return { ...initialState, ...action.payload, error: '', loading: false };
         },
         [login.rejected]: (state, action) => {
+            state.error = action.payload.error;
+            state.loading = false;
+        },
+        [updateAccount.fulfilled]: (state, action) => {
+            const { email, password } = action.payload.data;
+            return { ...initialState, email, password, error: '', loading: false };
+        },
+        [updateAccount.rejected]: (state, action) => {
             state.error = action.payload.error;
             state.loading = false;
         },
@@ -115,6 +111,6 @@ const userSlice = createSlice({
     }
 });
 
-export { login, register };
+export { login, register, updateAccount };
 
 export default userSlice.reducer;
